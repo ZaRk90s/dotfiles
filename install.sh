311b6507c8ea5e5022da649a723f2a8f84e5a819
Rustem Adrián <84082234+ZaRk90s@users.noreply.github.com>
2025-08-05 15:45:58 +0100

    Update script

--- install.sh
+++ install.sh
@@ -14,6 +14,16 @@ function helpPanel() {
 }
 
 function configuration() {
+    
+    # Take in count
+    echo -e "\n[+] Things you have to take in count:"
+    echo -e "\n1. If you are using ParrotOS install the packages manually, GitHub or install it using \"apt install\" but the versions it won't be the latest"
+    echo -e "\n2. You architecture of your processor (X86 or ARM64 (the most common))"
+    echo -e "\n3. You can change whatever you want in the /home/$username/.config\n"
+
+    # Update system
+    apt update && apt upgrade || { echo '[!] Error al actualizar el sistema'; exit 1; }
+
     # Download packages
     apt install zsh mono-devel bspwm sxhkd polybar feh rofi jq clangd exploitdb snmp exiftool steghide html2text radare2 lsd bat neovim
     
@@ -21,30 +31,34 @@ function configuration() {
     # apt install kitty
     
     # Download Hack Nerd Font
-    wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.4.0/Hack.zip; mv Hack.zip /usr/share/fonts
+    cd ~/Downloads
+    wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.4.0/Hack.zip
+    mv Hack.zip /usr/share/fonts/
     unzip /usr/share/fonts/Hack.zip
     rm -r /usr/share/fonts/Hack.zip
+    fc-cache -fv
 
     # Download dotfiles from ZaRk90s
-    mv ./{bin,bspwm,kitty,nvim,polybar,sxhkd} /home/$username/.config; mv ./{.p10k.zsh,.zshrc} /home/$username
+    git clone https://github.com/ZaRk90s/dotfiles
+    mv ./{bin,bspwm,kitty,nvim,polybar,sxhkd} "/home/$username/.config"; mv ./{.p10k.zsh,.zshrc} "/home/$username"
 
     # Download powerlevel10k
     git clone --depth=1 https://github.com/romkatv/powerlevel10k.git /home/$username/powerlevel10k
-    git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
+    git clone --depth=1 https://github.com/romkatv/powerlevel10k.git /root/powerlevel10k
 
     # Change zark90s for your username in the files
     grep -ri "$username" /home/$username/.config | grep -vE "nvim|xfce4|gtk-3.0|ghidra|qterminal.org" | cut -d: -f1 | uniq | xargs -d '\n' -I{} sed -i "s/zark90s/$username/g" "{}"
     
     # Same configuration for root user and username
-    ln -sf /home/$username/.zshrc ~/.zshrc
-    ln -sf /home/$username/.p10k.zsh ~/.p10k.zsh
+    ln -sf /home/$username/.zshrc /root/.zshrc
+    ln -sf /home/$username/.p10k.zsh /root/.p10k.zsh
 
     # Attention
     echo -e "\n[+] Things you have to change manually:"
     echo -e "\t1. /home/$username/.config/sxhkd/sxhkdrc location of the terminal"
     echo -e "\t2. /home/$username/.config/bspwm/bspwmrc location of the wallpapers"
 
-    echo -e "\n\n[++] Already installed all correctly. Change to BSPWM ;)\n\n"
+    echo -e "\n\n[++] Already installed all correctly. Change to BSPWM, enjoy it and happy Hacking ;)\n\n"
 }
 
 if [ "$(echo $UID)" -eq 0 ]; then
